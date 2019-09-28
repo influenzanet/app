42bb21f69ed1407a8e0bb8c8bbb84fb7bb249ace
Kevin Brendel <kBrendel@hotmail.de>
2019-09-28 22:12:34 +0200

    WIP theme and InstanceSelectionPage mockup.

--- lib/ui/common/influenzanet_theme.dart
+++ lib/ui/common/influenzanet_theme.dart
@@ -1,3 +1,57 @@
 import 'package:flutter/material.dart';
 
-ThemeData influenzaNetTheme = ThemeData(primarySwatch: Colors.blue);
+ThemeData influenzaNetTheme = ThemeData(
+    primarySwatch: ThemeElements.primarySwatch,
+    primaryColor: ThemeElements.primaryColor,
+    primaryColorLight: ThemeElements.primaryColorLight,
+    primaryColorDark: ThemeElements.primaryColorDark,
+    accentColor: ThemeElements.accentColor,
+    canvasColor: const Color(0xfff2f2f2),
+    textTheme: TextTheme(
+        display1: ThemeElements.displayTextStyle,
+        display2: ThemeElements.displayTextStyle,
+        display3: ThemeElements.displayTextStyle,
+        display4: ThemeElements.displayTextStyle),
+    buttonTheme: ButtonThemeData(
+        buttonColor: ThemeElements.primaryColor,
+        textTheme: ButtonTextTheme.primary,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(19.0))));
+
+class ThemeElements {
+  static const MaterialColor primarySwatch = MaterialColor(0xff4b669d, {
+    50: const Color(0xffe5e9f0),
+    100: const Color(0xffbec7dc),
+    200: const Color(0xff94a4c4),
+    300: const Color(0xff6c81ad),
+    400: const Color(0xff4b669d),
+    500: const Color(0xff284d8e),
+    600: const Color(0xff214686),
+    700: const Color(0xff173d7a),
+    800: const Color(0xff0f336e),
+    900: const Color(0xff042357),
+  });
+
+  static const MaterialColor accentSwatch = MaterialColor(0xff61b1b0, {
+    50: const Color(0xffe2f1f2),
+    100: const Color(0xffb7dddd),
+    200: const Color(0xff8ac7c7),
+    300: const Color(0xff61b1b0),
+    400: const Color(0xff49a09e),
+    500: const Color(0xff36908c),
+    600: const Color(0xff33837f),
+    700: const Color(0xff2e746f),
+    800: const Color(0xff29645f),
+    900: const Color(0xff1f4843),
+  });
+
+  static const Color primaryColor = const Color(0xff0f346e);
+  static const Color primaryColorLight = const Color(0xff475d9d);
+  static const Color primaryColorDark = const Color(0xff000e42);
+  static const Color accentColor = const Color(0xff61b1b0);
+  static const Color accentColorLight = const Color(0xff93e3e2);
+  static const Color accentColorDark = const Color(0xff2d8181);
+
+  static const TextStyle displayTextStyle = TextStyle(
+    color: primaryColor,
+  );
+}
--- lib/ui/onboarding/pages/instance-selection-page.dart
+++ lib/ui/onboarding/pages/instance-selection-page.dart
@@ -1,12 +1,96 @@
+import 'package:InfluenzaNet/ui/common/influenzanet_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class InstanceSelectionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(),
-      body: Text('Instance Selection'),
+      appBar: AppBar(
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          "Welcome to InfluenzaNet",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: themeData.primaryColor),
+        ),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Spacer(
+            flex: 15,
+          ),
+          Text(
+            'Select your Country',
+            style: themeData.textTheme.display1.apply(color: themeData.accentColor),
+            textAlign: TextAlign.center,
+          ),
+          Spacer(flex: 15),
+          Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                'Germany',
+                textAlign: TextAlign.center,
+                style: themeData.textTheme.title,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: RaisedButton(
+                  shape: CircleBorder(),
+                  color: Colors.grey,
+                  clipBehavior: Clip.antiAlias,
+                  onPressed: () {},
+                  child: Container(
+                    color: Colors.red,
+                    height: 175,
+                    child: Image.asset("assets/images/country-flags/germany.png"),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlatButton(
+                    child: Text(
+                      'Change Country',
+                    ),
+                    color: Colors.white,
+                    textColor: themeData.accentColor,
+                    splashColor: ThemeElements.accentSwatch[50],
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Spacer(flex: 20),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                color: themeData.primaryColor,
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+                child: Text(
+                  'Next',
+                  style: TextStyle(fontSize: 30.0),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          Spacer(flex: 10)
+        ],
+      ),
     );
   }
 }
