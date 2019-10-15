42235dc994216da7e4db9b806187a02c2023d107
Kevin Brendel <kBrendel@hotmail.de>
2019-10-15 23:47:34 +0200

    Added actions support to ThemedAppBar.

--- lib/ui/common/widgets/app-bars/themed-app-bar.dart
+++ lib/ui/common/widgets/app-bars/themed-app-bar.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ThemedAppBar extends AppBar {
-  factory ThemedAppBar(ThemeData themeData, {String titleText = ''}) {
-    return ThemedAppBar.widget(themeData,
-        title: Text(titleText,
-            textAlign: TextAlign.center,
-            //style: themeData.textTheme.display1.copyWith(color: themeData.primaryColor, fontSize: 30.0),
-            style: TextStyle(color: themeData.primaryColor)));
+  factory ThemedAppBar(ThemeData themeData, {String titleText = '', List<Widget> actions}) {
+    return ThemedAppBar.widget(
+      themeData,
+      title: Text(
+        titleText,
+        textAlign: TextAlign.center,
+        //style: themeData.textTheme.display1.copyWith(color: themeData.primaryColor, fontSize: 30.0),
+        style: TextStyle(color: themeData.primaryColor),
+      ),
+      actions: actions,
+    );
   }
 
-  ThemedAppBar.widget(ThemeData themeData, {Widget title}) : super(centerTitle: true, title: title);
+  ThemedAppBar.widget(ThemeData themeData, {Widget title, List<Widget> actions})
+      : super(centerTitle: true, title: title, actions: actions);
 }
