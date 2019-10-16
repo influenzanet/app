657772b6e68c3026a3ff21869b976d951fa924ef
Kevin Brendel <kBrendel@hotmail.de>
2019-10-16 13:34:41 +0200

    Added ProfilePage (logged in).

--- lib/ui/main/profile/profile-page.dart
+++ lib/ui/main/profile/profile-page.dart
@@ -1,4 +1,6 @@
+import 'package:InfluenzaNet/ui/common/themes/influenzanet-theme.dart';
 import 'package:InfluenzaNet/ui/common/widgets/app-bars/themed-app-bar.dart';
+import 'package:InfluenzaNet/ui/common/widgets/buttons/themed-flat-button.dart';
 import 'package:InfluenzaNet/ui/common/widgets/scaffolds/themed-scaffold.dart';
 import 'package:InfluenzaNet/ui/main/drawer/main-drawer.dart';
 import 'package:flutter/material.dart';
@@ -11,14 +13,43 @@ class ProfilePage extends StatelessWidget {
       appBar: ThemedAppBar(
         themeData,
         titleText: 'Profile',
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {},
-          )
-        ],
+        // actions: <Widget>[
+        //   IconButton(
+        //     icon: Icon(Icons.edit),
+        //     onPressed: () {},
+        //   )
+        // ],
       ),
       drawer: MainDrawer(),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            _avatar(themeData),
+            Container(height: ThemeElements.elementPadding),
+            Text(
+              'max.mustermann@muster.de',
+              style: themeData.textTheme.title,
+            ),
+            Container(height: ThemeElements.elementPadding),
+            Text(
+              '•••••••••••••••••••••',
+              style: themeData.textTheme.title,
+            ),
+            Container(height: ThemeElements.elementPadding),
+            ThemedFlatButton(themeData, text: 'Edit Profile'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _avatar(ThemeData themeData) {
+    return Container(
+      height: 170,
+      child: Image.asset('assets/images/avatars/default-avatar.png'),
     );
   }
 }
